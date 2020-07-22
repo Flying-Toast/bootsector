92ea97da0d3391292222a318439bfa7628839a09
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-21 20:32:14 -0400

    update qemu script

--- boot/qemu.sh
+++ boot/qemu.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if mkbootdisk main.s
+if [ -z $1 ]
+then
+	echo Bad arguments.
+	echo "    Usage: $(basename $0) <input.s>"
+	exit 1
+fi
+
+if mkbootdisk $1
 then
 	qemu-system-x86_64 -monitor stdio -drive format=raw,file=bootdisk.bin
+else
+	echo Failed because of above errors
 fi
